4b01e4a51e9be325ea5055b36f5ec4f41ca60a29
Adarsh Maurya <adarshmaurya1000@gmail.com>
2020-09-28 21:36:07 +0530

    cleared some bugs

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -72,13 +72,13 @@ public class Dashboard extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-              new Author_det().setVisible(true);
+              new Publisher_det().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-              new Author_det().setVisible(true);
+              new Book_det().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
